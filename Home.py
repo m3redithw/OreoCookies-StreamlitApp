923f5a5f9a1566b4dabf4bd70107446d1b8bd5f0
Meredith Wang <wang.meredith09@gmail.com>
2022-10-18 12:30:37 -0500

    update

--- Home.py
+++ Home.py
@@ -3,8 +3,12 @@ import pandas as pd
 import plotly.express as px
 import plotly.figure_factory as ff
 from PIL import Image
-st.title("What's Your Favorite Oreo?")
+
+text = '<p style=" color:#7f4718; font-size: 30px;"<b>Oreo Cookies</b></p>'
+st.markdown(text, unsafe_allow_html=True)
+
 st.markdown("by **Meredith Wang**")
 
-text = '<p style=" color:#7f4718; font-size: 50px;">by <b>Meredith Wang</b></p>'
-st.markdown(text, unsafe_allow_html=True)
+
+
+st.write('')
